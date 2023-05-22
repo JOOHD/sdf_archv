523d1e066804512559936b04fff3dde5728c20bf
JOOHD <hddong728@naver.com>
2023-05-22 17:51:28 +0900

    file upload

---
+++ sdf_archv/src/test/java/sdf_archv/sdf_archv/SdfArchvApplicationTests.java
@@ -0,0 +1,13 @@
+package sdf_archv.sdf_archv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdfArchvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
